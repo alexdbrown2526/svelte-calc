ab1e788c58aee97366c8a6a318b35ab78201dc9a
Alex Brown <alexdbrown2526@gmail.com>
2022-01-25 21:17:10 -0600

    svelte template created

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+</script>
+
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
+
+<h1>hello {name}</h1>
+<i class="fas fa-home fa-5x"/>
